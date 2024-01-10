bf1bbe851cf27af9cfaa170c99c4dcb60de167db
薛宇翔 BillyHsueh <BillyHsueh@altek.com.tw>
2024-01-10 13:18:55 +0800

    show architecture

--- models/yolo.py
+++ models/yolo.py
@@ -421,19 +421,26 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cfg", type=str, default="yolov5s.yaml", help="model.yaml")
+    # (+/-) -> modfy by billy: yolov5s.yaml -> yolov5altek.yaml
+    parser.add_argument("--cfg", type=str, default="yolov5altek.yaml", help="model.yaml")
+    # <- (+/-) modfy by billy:
     parser.add_argument("--batch-size", type=int, default=1, help="total batch size for all GPUs")
     parser.add_argument("--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu")
     parser.add_argument("--profile", action="store_true", help="profile model speed")
     parser.add_argument("--line-profile", action="store_true", help="profile model speed layer by layer")
     parser.add_argument("--test", action="store_true", help="test all yolo*.yaml")
+    # (+) -> add by billy
+    parser.add_argument("--img-size", type=int, default=320, help="input size")
+    # <- (+) add by billy
     opt = parser.parse_args()
     opt.cfg = check_yaml(opt.cfg)  # check YAML
     print_args(vars(opt))
     device = select_device(opt.device)
 
     # Create model
-    im = torch.rand(opt.batch_size, 3, 640, 640).to(device)
+    # (+/-) -> modfy by billy: 640 -> opt.img_size
+    im = torch.rand(opt.batch_size, 3, opt.img_size, opt.img_size).to(device)
+    # <- (+/-) modfy by billy
     model = Model(opt.cfg).to(device)
 
     # Options
@@ -452,3 +459,7 @@ if __name__ == "__main__":
 
     else:  # report fused model summary
         model.fuse()
+
+    # (+) -> add by billy
+    model.info(img_size=opt.img_size)
+    # <- (+) add by billy
